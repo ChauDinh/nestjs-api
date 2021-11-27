ae1c808f4a7017d3fe6eb4482c9dba18af8ef3b4
baochau.dinh <baochau.dinh@vexere.com>
2021-11-27 11:01:16 +0700

    develop: create posts controller, posts service and posts module

---
+++ src/posts/posts.service.ts
@@ -0,0 +1,50 @@
+import { HttpException, HttpStatus, Injectable } from '@nestjs/common';
+import CreatePostDto from './dto/createPost.dto';
+import UpdatePostDto from './dto/updatePost.dto';
+import { Post } from './post.interface';
+
+@Injectable()
+export default class PostsService {
+  private lastPostId = 0;
+  private posts: Post[] = [];
+
+  getAllPosts(): Post[] {
+    return this.posts;
+  }
+
+  getPostById(id: number): Post {
+    const post = this.posts.find((post) => post.id === id);
+    if (post) {
+      return post;
+    }
+    throw new HttpException('Post not found', HttpStatus.NOT_FOUND);
+  }
+
+  replacePost(id: number, post: UpdatePostDto): Post {
+    const postIdx = this.posts.findIndex((post) => post.id === id);
+    if (postIdx > -1) {
+      this.posts[postIdx] = post;
+      return post;
+    }
+    throw new HttpException('Post not found', HttpStatus.NOT_FOUND);
+  }
+
+  createPost(post: CreatePostDto) {
+    const newPost = {
+      id: ++this.lastPostId,
+      ...post,
+    };
+    this.posts.push(newPost);
+    return newPost;
+  }
+
+  deletePost(id: number) {
+    const postIdx = this.posts.findIndex((post) => post.id === id);
+    if (postIdx > -1) {
+      this.posts.splice(postIdx, 1);
+    } else {
+      throw new HttpException('Post not found', HttpStatus.NOT_FOUND);
+    }
+    return;
+  }
+}
